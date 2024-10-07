4537709472ca25b41fa6b607dfaf6a4e50643166
Joaquín Sacha Varisco <varisco54@gmail.com>
2024-10-07 13:25:14 -0300

    ChangeSounds

--- Assets/Scripts/Player/Player_Jump.cs
+++ Assets/Scripts/Player/Player_Jump.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player_Jump : MonoBehaviour
 {
+    [Header ("Land")]
+    private bool toLand;
+
     [Header ("Feets")]
     [SerializeField] private Transform feetsPos;
 
@@ -18,23 +21,27 @@ public class Player_Jump : MonoBehaviour
         playerData = GetComponent<Player_Controller>().playerData;
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        toLand = false;
     }
 
     private void Update() {
         bool isGrounded = Physics2D.OverlapCircle(feetsPos.position, playerData.feetRadius, playerData.ground);
         animator.SetFloat("MoveY", rb2D.velocity.y); 
         animator.SetBool("IsGrounded", isGrounded);
-
         
         if (Input.GetKeyDown(playerData.jumpKey) && isGrounded)
         {
+            toLand = true;
             Audio_Controller.Instance.PlaySound(playerData.jumpSound);
             rb2D.AddForce(Vector2.up * playerData.jumpForce);
         }
+        if (isGrounded && toLand && rb2D.velocity.y < 0)
+        {
+            toLand = false;
+            Audio_Controller.Instance.PlaySound(playerData.landSound);
+
+        }
     }
 
-    /*private void OnDrawGizmos() {
-        Gizmos.DrawWireSphere(feetsPos.position, playerData.feetRadius);
-
-    }*/
+  
 }
